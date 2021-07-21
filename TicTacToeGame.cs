0f127f1af41124b80d5f67054e809f4f2b8b1072
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-21 11:54:49 +0530

    [Ravina] Add: Creating the empty board

---
+++ TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+
+        public static void CreateBoard()
+        {
+            char[] Board = new char  [10];
+            char[] Board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+
+
+
+
+    }
+}
